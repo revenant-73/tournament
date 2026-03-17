c08c40e7b3e4c25ae049480e61662b712dfc4084
Loren <loren@example.com>
2026-03-17 13:51:34 -0700

    "Deploy"

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
-import { defineConfig } from 'vite'  
-import react from '@vitejs/plugin-react-swc'  
-export default defineConfig({ plugins: [react()] }) 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/tournament/', // Replace with your repo name
+})
